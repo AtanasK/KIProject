197ba6463a014531883aafbb574bdd066d32bad3
Ferid Memed <ferid.dragon@hotmail.com>
2016-08-23 14:02:39 +0200

    Logo activity = done

---
+++ app/src/main/java/com/feridgoranatanas/projectpinkifinki/LogoAnimation.java
@@ -0,0 +1,98 @@
+package com.feridgoranatanas.projectpinkifinki;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class LogoAnimation extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_animation);
+
+        GIFView imageView = (GIFView) findViewById(R.id.imageView);
+        imageView.loadGIFResource(this, R.raw.logo_animation);
+
+        getSupportActionBar().hide();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        waitAndStartMain();
+    }
+
+    private void waitAndStartMain() {
+
+        //Wait for 3 seconds
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long currentTime = android.os.SystemClock.uptimeMillis();
+                while(android.os.SystemClock.uptimeMillis() - currentTime < 3000) {
+                    //do nothing
+                }
+
+                //After waiting for 3 seconds redirect to main page
+                Intent main = new Intent(LogoAnimation.this, MainActivity.class);
+                startActivity(main);
+            }
+        });
+
+        //Start thread
+        t.start();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LogoAnimation Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.feridgoranatanas.projectpinkifinki/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LogoAnimation Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.feridgoranatanas.projectpinkifinki/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
