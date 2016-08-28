39531a8b6477c939a9cfa57e73f25feb0847d428
Ferid Memed <ferid.dragon@hotmail.com>
2016-08-28 16:30:56 +0200

    RunStatsActivity interface = done

--- app/src/main/java/com/feridgoranatanas/projectpinkifinki/MapsActivity.java
+++ app/src/main/java/com/feridgoranatanas/projectpinkifinki/MapsActivity.java
@@ -3,19 +3,18 @@ package com.feridgoranatanas.projectpinkifinki;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity  extends FragmentActivity {
 
-    private GoogleMap mMap;
+    private SupportMapFragment mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run_stats);
+        setContentView(R.layout.google_maps_api);
         setUpMapIfNeeded();
     }
 
@@ -27,7 +26,7 @@ public class MapsActivity  extends FragmentActivity {
 
     private void setUpMapIfNeeded() {
         if(mMap == null) {
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            mMap = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
             if(mMap != null) {
                 setUpMap();
@@ -38,7 +37,7 @@ public class MapsActivity  extends FragmentActivity {
 //    Ovde kje se setiraat markerite na mapata treba samo brojkite kaj LatLng shto se da bidat nekoi promenlivi sho kje se chitaat i kje se vnesuvaat, ja neam blage veze kako bi go napraile toa
 //            bash na ovoj chekor zaglavivme na nasa :/
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.getMap().addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Start"));
     }
 
 }
--- app/src/main/java/com/feridgoranatanas/projectpinkifinki/RunStatsActivity.java
+++ app/src/main/java/com/feridgoranatanas/projectpinkifinki/RunStatsActivity.java
@@ -1,7 +1,10 @@
 package com.feridgoranatanas.projectpinkifinki;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RunStatsActivity extends AppCompatActivity {
@@ -12,7 +15,33 @@ public class RunStatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_run_stats);
 
         Bundle bundle = getIntent().getExtras();
-        TextView testView = (TextView)findViewById(R.id.textTime);
+        TextView testView = (TextView)findViewById(R.id.textStats);
         testView.setText(String.format("%d", bundle.getInt("time")));
+
+        Button button = (Button)findViewById(R.id.btnAllRuns);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent allRuns = new Intent(RunStatsActivity.this, AllRunsActivity.class);
+                startActivity(allRuns);
+            }
+        });
+
+        button = (Button)findViewById(R.id.btnGetB);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(RunStatsActivity.this, MainActivity.class);
+                startActivity(main);
+            }
+        });
+
+        button = (Button)findViewById(R.id.btnShare);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Tuka kje bide sharingot
+            }
+        });
     }
 }
