2c7511f86f21fa0c3af3e79b74524a7c48bfd45f
Ferid Memed <ferid.dragon@hotmail.com>
2016-08-29 20:33:57 +0200

    Fixed map, path drawing, markers..

--- app/src/main/java/com/feridgoranatanas/projectpinkifinki/RunStatsActivity.java
+++ app/src/main/java/com/feridgoranatanas/projectpinkifinki/RunStatsActivity.java
@@ -1,17 +1,29 @@
 package com.feridgoranatanas.projectpinkifinki;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 
-public class RunStatsActivity extends AppCompatActivity {
+public class RunStatsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private ArrayList<LatLng> coordsList;
+    private SupportMapFragment supportMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +32,11 @@ public class RunStatsActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         TextView testView = (TextView)findViewById(R.id.textStats);
-        ArrayList<LatLng> coordsList = (ArrayList<LatLng>)bundle.get("coords");
+        coordsList = (ArrayList<LatLng>)bundle.get("coords");
         testView.setText(String.format("%d \n Last Coord: %f, %f", bundle.getInt("time"), coordsList.get(0).latitude, coordsList.get(0).longitude));
+
+        supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
 
         Button button = (Button)findViewById(R.id.btnAllRuns);
         button.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +63,21 @@ public class RunStatsActivity extends AppCompatActivity {
                 //Tuka kje bide sharingot
             }
         });
+    }
+
+    public void drawLine(ArrayList<LatLng> coords, GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(coords.get(0)).title("Start"));
+        Polyline line = map.addPolyline(new PolylineOptions()
+                .addAll(coords)
+                .width(5)
+                .color(Color.parseColor("#262626")));
+        map.addMarker(new MarkerOptions().position(coords.get(coords.size() - 1)).title("End"));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        drawLine(coordsList, googleMap);
+        LatLngBounds latLngBounds = new LatLngBounds(coordsList.get(0), coordsList.get(coordsList.size() - 1));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngBounds.getCenter(), 19));
     }
 }
